8570eb10861a0f13c6bbbc38db981742702954ba
Phil Jones <phil.jones88@gmail.com>
2012-02-18 10:50:50 +0000

    Tabs -> spaces and formatting

--- jquery.jElementExpander.js
+++ jquery.jElementExpander.js
@@ -1,7 +1,7 @@
 /*!
-* jQuery jElementExpander Plugin v1.0.0
+* jQuery jElementExpander Plugin v1.0.1
 *
-* Date: Fri Feb 17 20:28:11 2012 GTM
+* Date: Fri Feb 18 10:49:17 2012 GTM
 * Requires: jQuery v1.3+
 *
 * Copyright 2012, Philip Jones (@philjones88)
@@ -11,55 +11,55 @@
 */
 (function( $ ){
 
-  $.fn.jElementExpander = function( options ) {  
+    $.fn.jElementExpander = function( options ) {  
 
-    var settings = $.extend( {
-      elementsToBreatAt : ['H2', 'UL'],
-      hiddenLabel: 'Read more...',
-      showLabel: 'Read less...'
-    }, options);
+        var settings = $.extend( {
+            elementsToBreatAt : ['H2', 'UL'],
+            hiddenLabel: 'Read more...',
+            showLabel: 'Read less...'
+        }, options);
 
-  return this.each(function() {
-    var $this = $(this);
+        return this.each(function() {
+            var $this = $(this);
 
-    var breakElementBeen = false;
-    var hiddenElements = $('<div class="hidden-elements"></div>');
+            var breakElementBeen = false;
+            var hiddenElements = $('<div class="hidden-elements"></div>');
 
-    $this.children().each(function(i, item) {  
+            $this.children().each(function(i, item) {  
 
-    	if (jQuery.inArray(item.tagName, settings.elementsToBreatAt) != -1) {
-    		breakElementBeen = true;
+                if (jQuery.inArray(item.tagName, settings.elementsToBreatAt) != -1) {
+                    breakElementBeen = true;
 
-    		hiddenElements.append(item);
-    	} else	if (breakElementBeen){
-    			hiddenElements.append(item);
-    		}
-    });
+                    hiddenElements.append(item);
+                } else  if (breakElementBeen) {
+                    hiddenElements.append(item);
+                }
+            });
 
-    if (breakElementBeen){
-    	hiddenElements.hide();
-    	$this.append(hiddenElements);
-    	$this.append(buildReadMore(hiddenElements));
-    }
-  });
+            if (breakElementBeen) {
+                hiddenElements.hide();
+                $this.append(hiddenElements);
+                $this.append(buildReadMore(hiddenElements));
+            }
+        });
 
-    function buildReadMore(hidden){
-    	var readmore = $('<a href="javascript:void(0)">' + settings.hiddenLabel + '</a>');
+        function buildReadMore(hidden){
+            var readmore = $('<a href="javascript:void(0)">' + settings.hiddenLabel + '</a>');
 
-    	readmore.click(function(){
-        var $this = $(this);
+            readmore.click(function() {
+                var $this = $(this);
 
-        if ($this.text() == settings.hiddenLabel){
-    			$this.text(settings.showLabel);
-    		}else{
-    			$this.text(settings.hiddenLabel);
-    		}
+                if ($this.text() == settings.hiddenLabel){
+                    $this.text(settings.showLabel);
+                }else{
+                    $this.text(settings.hiddenLabel);
+                }
 
-    		hidden.toggle();
-    	});
+                hidden.toggle();
+            });
 
-    	return readmore;
-    }
+            return readmore;
+        }
 
-  };
+    };
 })( jQuery );
